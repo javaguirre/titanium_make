b3773bc1ef9e78d698abe1dd3b887cc219ff5a8d
Javier Aguirre <contacto@javaguirre.net>
2012-06-19 21:09:44 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+PROJECT_NAME=example
+PROJECT_ROOT=$(shell pwd)
+
+run-iphone:
+	@DEVICE_TYPE=iphone make run
+
+test-iphone:
+	@DEVICE_TYPE=iphone make test
+
+run-ipad:
+	@DEVICE_TYPE=ipad make run
+
+test-ipad:
+	@DEVICE_TYPE=ipad make test
+
+run-android:
+	@DEVICE_TYPE=android make run
+
+test-android:
+	@DEVICE_TYPE=android make test
+
+run:
+	@if [ "${DEVICE_TYPE}" == "" ]; then\
+		echo "Please run \"make run-[iphone|ipad]\" instead.";\
+		exit 1;\
+	fi
+	@make launch-titanium
+
+test:
+	@if [ "${DEVICE_TYPE}" == "" ]; then\
+		echo "Please run \"make test-[iphone|ipad]\" instead.";\
+		exit 1;\
+	fi
+	@make launch-titanium
+
+clean:
+	@rm -rf ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/*
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/
+	@echo "Deleted: ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/*"
+
+launch-titanium:
+	@echo "Building with Titanium... (DEVICE_TYPE:${DEVICE_TYPE})"
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/
+	@PROJECT_NAME=${PROJECT_NAME} PROJECT_ROOT=${PROJECT_ROOT} DEVICE_TYPE=${DEVICE_TYPE} bash ${PROJECT_ROOT}/titanium.sh
---
+++ titanium.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Utility script to start Titanium Mobile project from the command line.
+# More info at http://github.com/guilhermechapiewski/titanium-jasmine
+
+# Customize these variables
+TI_SDK_VERSION="2.0.2.GA"
+TI_DIR="$HOME/.titanium"
+IPHONE_SDK_VERSION="4.2"
+ANDROID_API_LEVEL="23" #the API level of the targeted emulator
+ANDROID_SDK_PATH="$HOME/apps/android-sdk-linux" #example path from homebrew install
+
+PROJECT_NAME=${PROJECT_NAME}
+PROJECT_ROOT=${PROJECT_ROOT:-../}
+APP_DEVICE=${DEVICE_TYPE}
+TI_ASSETS_DIR="${TI_DIR}/mobilesdk/linux/${TI_SDK_VERSION}" # Both iOS and Android SDKs are linked in this directory
+
+# iOS settings
+TI_IPHONE_DIR="${TI_ASSETS_DIR}/iphone"
+TI_IPHONE_BUILD="${TI_IPHONE_DIR}/builder.py"
+
+# Android settings
+TI_ANDROID_DIR="${TI_ASSETS_DIR}/android"
+TI_ANDROID_BUILD="${TI_ANDROID_DIR}/builder.py"
+
+if [ "PROJECT_NAME" == "" ]; then
+	echo "[ERROR] Please inform PROJECT_NAME."
+	exit 1
+fi
+
+if [ "DEVICE_TYPE" == "" ]; then
+	echo "[ERROR] Please inform DEVICE_TYPE ('ipad' or 'iphone' or 'android')."
+	exit 1
+fi
+
+# Get APP parameters from current tiapp.xml
+APP_ID=`cat ${PROJECT_ROOT}/${PROJECT_NAME}/tiapp.xml | grep "<id>" | sed -e "s/<\/*id>//g"`
+APP_NAME=`cat ${PROJECT_ROOT}/${PROJECT_NAME}/tiapp.xml | grep "<name>" | sed -e "s/<\/*name>//g"`
+
+if [ "APP_ID" == "" ] || [ "APP_NAME" == "" ]; then
+	echo "[ERROR] Could not obtain APP parameters from tiapp.xml file (does the file exist?)."
+	exit 1
+fi
+
+if [ ${APP_DEVICE} == "iphone" ] || [ ${APP_DEVICE} == "ipad" ]; then
+	killall "iPhone Simulator"
+	bash -c "python2 ${TI_IPHONE_BUILD} run ${PROJECT_ROOT}/${PROJECT_NAME}/ ${IPHONE_SDK_VERSION} ${APP_ID} ${APP_NAME} ${APP_DEVICE}"
+elif [ ${APP_DEVICE} == "android" ]; then
+	# Check for Android Virtual Device (AVD)
+	if [ "$(pidof emulator-arm)" ]
+	  then
+	  	echo "[INFO] Emulator already running, going to launch with that."
+	  else
+	  	echo "[ERROR] Could not find a running emulator."
+	  	echo "[ERROR] Run this command in a separate terminal session: ${ANDROID_SDK_PATH}/tools/emulator-arm -avd android${ANDROID_API_LEVEL}"
+	  	exit 1
+	fi
+	bash -c "python2 ${TI_ANDROID_BUILD} simulator ${APP_NAME} ${ANDROID_SDK_PATH} ${PROJECT_ROOT}/${PROJECT_NAME}/ ${APP_ID} ${ANDROID_API_LEVEL}"
+else
+	echo "${APP_DEVICE} is not supported. Only iphone, ipad, and android."
+	exit 1
+fi
+
+perl -pe 's/^\[DEBUG\].*$/\e[35m$&\e[0m/g;s/^\[INFO\].*$/\e[36m$&\e[0m/g;s/^\[WARN\].*$/\e[33m$&\e[0m/g;s/^\[ERROR\].*$/\e[31m$&\e[0m/g;'
